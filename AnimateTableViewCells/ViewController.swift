ddf77efdc4d1ac5cb2a2ef3c5ea258c034c5f492
kazim <kazim.ahmad@rocketmail.com>
2019-02-18 13:18:46 +0000

    Initial Commit

---
+++ AnimateTableViewCells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimateTableViewCells
+//
+//  Created by KazimAhmad on 18/02/2019.
+//  Copyright © 2019 self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
